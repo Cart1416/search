fa244354df677388c4123412c0c80f33bf420adc
Cart1416 <83319239+Cart1416@users.noreply.github.com>
2023-12-09 22:36:02 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,60 @@
-alert("works!");
+/// execute_script.js
+const secretMenu = {
+help: "Show all commands",
+secretCommand1: "Perform secret command 1",
+darkmode: "Enable Dark mode for this page",
+// Add more secret commands as needed
+};
+
+function menu() {
+  let userInput;
+  do {
+    userInput = prompt("Enter a command (Type 'help' for a list of commands):");
+    if (userInput) {
+      processCommand(userInput);
+    }
+  } while (userInput !== null);
+}
+
+function processCommand(command) {
+  switch (command) {
+    case "help":
+      showHelp();
+      break;
+    case "secretCommand1":
+      performSecretCommand1();
+      break;
+    case "secretCommand2":
+      performSecretCommand2();
+      break;
+    // Add more cases for additional commands
+    default:
+      alert("Invalid command. Type 'help' for a list of commands.");
+  }
+}
+
+function showHelp() {
+  let helpMessage = "Available Commands:\n";
+  for (const key in secretMenu) {
+    helpMessage += `${key}: ${secretMenu[key]}\n`;
+  }
+  alert(helpMessage);
+}
+
+function performSecretCommand1() {
+  alert("Executing secret command 1...");
+}
+
+function darkmode() {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    document.querySelector('p').style.color = '#b3b3b3';
+    document.querySelector('a').style.color = '#4db8ff';
+    document.querySelector('h1').style.color = '#ffcc66';
+    alert("Dark Mode successful");
+}
+window.addEventListener("keyup", event => {
+    if (event.ctrlKey && event.which === 192) {
+        menu();
+    }
+})
